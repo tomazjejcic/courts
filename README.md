695ae819e8bc6d9777e4d6e5e86431c9f44e2749
Tomaz Jejcic <tomazjejcic@gmail.com>
2017-09-27 22:14:33 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# meanauthapp
+
+### Installing project locally
+```bash
+npm install
+```
+
+### running project locally
+```bash
+npm start
+```
---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const config = require('./config/database');
+
+// Connect to database
+mongoose.connect(config.database, {useMongoClient: true});
+
+// Connection success
+mongoose.connection.on('connected', () => {
+    console.log('Connected to database: ', config.database);
+})
+
+// Connection error
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ', err);
+})
+
+const app = express();
+
+const users = require('./routes/users');
+
+const port = 3000;
+
+// CORS Middleware:  to make api request from diferent domain name
+app.use(cors());
+
+// Set Static Folder: where client side will go
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Body Parser Middleware: parses incoming request bodies
+app.use(bodyParser.json());
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./config/passport')(passport);
+
+app.use('/users', users);
+
+// Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+})
+
+// Start Server
+app.listen(port, () => {
+    console.log('Server started on port: ', port);
+});
